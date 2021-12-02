5bd8428bdbcc7b52a1b3cf41eb62d2dece46a76e
fu123 <1179941534@qq.com>
2021-12-02 20:44:43 +0800

    first

---
+++ src/views/Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <el-form rules="rules" ref="" :model="LoginForm" class="loginContainer">
+      <h3 class="loginTitle">系统登录</h3>
+      <el-form-item>
+        <el-input
+          v-model="LoginForm.username"
+          placeholder="请输入用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input
+          v-model="LoginForm.password"
+          placeholder="请输入密码"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input
+          type="text"
+          v-model="LoginForm.code"
+          placeholder="点击图片更换验证码"
+          style="width: 250px; margin-right: 5px"
+        ></el-input>
+        <img :src="captchaUrl" />
+      </el-form-item>
+
+      <el-checkbox v-model="checked" class="loginRember">记住我</el-checkbox>
+      <el-button type="primary" style="width: 100%" @click="submitLogin" class="sfsdf"
+        >登录</el-button
+      >
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      captchaUrl: '',
+      LoginForm: {
+        username: 'admin',
+        password: '123',
+        code: '',
+      },
+      checked: true,
+      rules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur',
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    submitLogin() {
+      alert(111)
+    },
+  },
+}
+</script>
+
+<style>
+.loginContainer {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 180px auto;
+  width: 350px;
+  padding: 15px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
+
+.loginTitle {
+  margin: 0px auto 40px auto;
+  text-align: center;
+}
+.loginRember {
+  text-align: left;
+  margin: 0px 0px 15px 0px;
+}
+.asd {
+  scroll-behavior: auto;
+}
+</style>
